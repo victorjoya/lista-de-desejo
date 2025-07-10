c34415810b437a9c5e20e1b254b0cd32032e6f24
Victor Hugo Joya Braga <PET0159842@aluno.firjansenaisesi.com.br>
2025-07-09 21:32:33 -0300

    nova

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,28 @@
+import { Navigate, useNavigate } from "react-router";
 import styles from "./Card.module.css";
 
 export const Card = ({ name, description, image, date, onDelete }) => {
-    const [ano, mes, dia] = date.split("-");
-    const convertedDate = new Date(ano, mes - 1, dia);
-    const formatedDate = isNaN(convertedDate)
-        ? ""
-        : Intl.DateTimeFormat("pt-Br").format(convertedDate);
+  const [ano, mes, dia] = date.split("-");
+  const convertedDate = new Date(ano, mes - 1, dia);
+  const formatedDate = isNaN(convertedDate)
+    ? ""
+    : Intl.DateTimeFormat("pt-Br").format(convertedDate);
 
-    return (
-        <div className={styles.card}>
-            <div className={styles.imgContainer}>
-                <img src={image} alt={name} className={styles.image} />
-            </div>
-            <div className={styles.content}>
-                <h3 className={styles.name}>{name}</h3>
-                <p className={styles.description}>{description}</p>
-                <p className={styles.description}>{formatedDate}</p>
-                <button className={styles.delete} onClick={onDelete}>
-                    🗑️
-                </button>
-            </div>
-        </div>
-    );
+  const Navigate = useNavigate();
+  return (
+    <div className={styles.card}>
+      <div className={styles.imgContainer}>
+        <img src={image} alt={name} className={styles.image} />
+      </div>
+      <div className={styles.content}>
+        <h3 className={styles.name}>{name}</h3>
+        <p className={styles.description}>{description}</p>
+        <p className={styles.description}>{formatedDate}</p>
+        <button className={styles.delete} onClick={onDelete}>
+          🗑️
+        </button>
+        <button onClick={Navigate("lista/" + id)}>Detalhes </button>
+      </div>
+    </div>
+  );
 };
